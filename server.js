7770cc2e6848ed80507730ebd14873d0ffb0d96a
tendinginfinity24 <tendinginfinity24@gmail.com>
2026-05-31 14:47:05 +0530

    port change

--- server.js
+++ server.js
@@ -233,4 +233,8 @@ app.post('/api/login', async (req, res) => {
     } catch (err) { res.status(500).json({ message: "Error" }); }
 });
 
-app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
